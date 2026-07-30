2f17b62d8545038c966995026cef7b468aba9265
luisale16 <luis.ale1695@gmail.com>
2026-07-30 14:51:34 -0500

    update rutas

--- src/app/services/Cobranza.service.ts
+++ src/app/services/Cobranza.service.ts
@@ -557,7 +557,7 @@ export class CobranzaService {
     //REAPERTURA
 
     registraReapertura(model: GestionCorte): Observable<Mensaje> {//https://gateway8054.emapasalas.net.pe
-        return this.http.post<Mensaje>(`http://localhost:8054/Cobranza/registraReapertura`, model);
+        return this.http.post<Mensaje>(`https://gateway8054.emapasalas.net.pe/Cobranza/registraReapertura`, model);
     }
 
     reporteCore(model: ReporteCore): Observable<ListResponse<ReporteCore[]>> {
